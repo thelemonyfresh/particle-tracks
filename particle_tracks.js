c9c122a74f0a56b3f4477977cea606f2f66d1798
Daniel Porter <daniel@pakled.local>
2019-06-13 20:22:55 +0200

    Protons, muons, electrons, photons...

--- particle_tracks.js
+++ particle_tracks.js
@@ -7,19 +7,21 @@ var ParticleTracks = {};
 
 // Set up drawing canvas.
 window.onload = function() {
-  console.log("doc loaded");
   ParticleTracks.width = 0;
   ParticleTracks.height = 0;
 
-  ParticleTracks.svg = document.getElementById("particle-tracks-svg");
   ParticleTracks.filterTurbulence = filterTurbulence = document.getElementById('bg-turbulence');
   ParticleTracks.canvas = document.getElementById("particle-tracks-canvas");
   ParticleTracks.ctx = prepareCanvas(ParticleTracks.canvas);
   ParticleTracks.dpRatio = window.devicePixelRatio;
 
+  ParticleTracks.ctx.fillStyle = "yellow";
+  ParticleTracks.ctx.fillRect(0, 0, ParticleTracks.canvas.width, ParticleTracks.canvas.height);
+
   setInterval(drawPaths,50);
-  setInterval(updateBg,250);
-  //setInterval(fadeOut,10000);
+  setInterval(updateBg,500);
+  setInterval(fadeOut,500);
+  setInterval(createParticle,1000);
 };
 
 function prepareCanvas(canvasElem) {
@@ -43,9 +45,13 @@ function prepareCanvas(canvasElem) {
 }
 
 // Drawing functions.
-function draw(x_i, y_i, x_f, y_f, lineWidth) {
+function draw(x_i, y_i, x_f, y_f, mass) {
+  // don't draw massless particles
+  if (mass == 0) return;
   let ctx = ParticleTracks.ctx;
-  ctx.lineWidth = lineWidth;
+  let lineWidth = 10 + (0.0075 * mass);
+  //console.log(`lw: ${lineWidth}`);
+  //ctx.lineWidth = lineWidth;
   ctx.lineCap = 'round';
   ctx.beginPath();
 
@@ -86,22 +92,21 @@ function particleTrackGradient(x_i,x_f,y_i,y_f,lineWidth) {
   gradient.addColorStop(0.75 + Math.random()*0.1,'black');
   gradient.addColorStop(1,'#ffffff00');
 
+
   return gradient;
 };
 
 function fadeOut() {
-  //ParticleTracks.ctx.fillStyle = 'transparent';//"rgba(255,255,255,0.1)";
-  //ParticleTracks.ctx.fillRect(0, 0, ParticleTracks.canvas.width, ParticleTracks.canvas.height);
-  console.log("fo");
-  console.log(ParticleTracks.svg);
-  ParticleTracks.ctx.globalAlpha = 0.5;
-  ParticleTracks.ctx.drawImage(ParticleTracks.svg, 0, 0);
+  ParticleTracks.ctx.fillStyle = "rgba(255,255,0,0.175)";
+  ParticleTracks.ctx.fillRect(0, 0, ParticleTracks.canvas.width, ParticleTracks.canvas.height);
+  //ParticleTracks.ctx.globalAlpha = 0.5;
+  //ParticleTracks.ctx.drawImage(ParticleTracks.svg, 0, 0);
 }
 
 function drawPaths() {
   ParticleTracks.activeParticles.forEach(part => {
     part.updatePosition();
-    draw(part.x,part.y,part.x_f,part.y_f, 10);
+    draw(part.x,part.y,part.x_f,part.y_f, part.mass);
   });
 };
 
@@ -139,6 +144,37 @@ class Particle {
   };
 
   updatePosition() {
+    console.log(`x: ${this.x}, y: ${this.y}`);
+    console.log(`vx: ${this.v_x}, vy: ${this.v_y}`);
+
+    this.x = this.x_f;
+    this.y = this.y_f;
+
+    // update the position
+    this.x_f = this.x + this.v_x;
+    this.y_f = this.y + this.v_y;
+
+    // don't update velocity for massless particles
+    if (this.mass != 0) {
+
+      // update the velocity
+      let chargeToMassRatio = this.charge/this.mass;
+      let drag = 0.0025 * (1/this.mass) * (Math.pow(this.v_x, 2) + Math.pow(this.v_y, 2));
+      let delta_v_x = this.chargeToMassRatio * this.v_y * 0.05;
+      let delta_v_y = -1 * this.chargeToMassRatio * this.v_x * 0.05;
+
+      //console.log(`abs vel: ${ this.absVelocity() } `);
+      //console.log(`decay: ${decay}`);
+      //console.log(`delt vx: ${delta_v_x}`);
+      //console.log(`delt vy: ${delta_v_y}`);
+
+      this.v_x = this.v_x + delta_v_x - (drag * this.v_x / this.absVelocity());
+      this.v_y = this.v_y + delta_v_y - (drag * this.v_y / this.absVelocity());
+    }
+    // possibly particle decay
+    if(this.decayProbability != undefined && this.decayProbability > Math.random()){
+      this.decay();
+    }
 
     let out_of_x = this.x > 2 * ParticleTracks.width || this.x < -1 * ParticleTracks.width;
     let out_of_y = this.y > 2* ParticleTracks.height || this.y < -1 * ParticleTracks.height;
@@ -148,30 +184,6 @@ class Particle {
       let index = ParticleTracks.activeParticles.indexOf(this);
       ParticleTracks.activeParticles.splice(index,1);
     }
-    console.log(`x: ${this.x}, y: ${this.y}`);
-    console.log(`vx: ${this.v_x}, vy: ${this.v_y}`);
-
-    this.x = this.x_f;
-    this.y = this.y_f;
-
-    // update velocity
-    let chargeToMassRatio = this.charge/this.mass;
-
-    this.x_f = this.x + this.v_x;
-    this.y_f = this.y + this.v_y;
-
-    let decay = 0.0025 * (1/this.mass) * (Math.pow(this.v_x, 2) + Math.pow(this.v_y, 2));
-
-    let delta_v_x = this.chargeToMassRatio * this.v_y * 0.05;
-    let delta_v_y = -1 * this.chargeToMassRatio * this.v_x * 0.05;
-
-    console.log(`abs vel: ${ this.absVelocity() } `);
-    console.log(`decay: ${decay}`);
-    //console.log(`delt vx: ${delta_v_x}`);
-    //console.log(`delt vy: ${delta_v_y}`);
-
-    this.v_x = this.v_x + delta_v_x - (decay * this.v_x / this.absVelocity());
-    this.v_y = this.v_y + delta_v_y - (decay * this.v_y / this.absVelocity());
 
   };
 };
@@ -191,12 +203,70 @@ class Positron extends Particle {
 class Muon extends Particle {
   constructor(pos_x, pos_y, vel_x, vel_y){
     super(pos_x, pos_y, vel_x, vel_y, -1, 1000);
+    this.decayProbability = 0.05;
   }
 
   // decays into one electron
+  decay() {
+    let e = new Electron(this.x, this.y, this.v_x, this.v_y);
+    ParticleTracks.activeParticles.push(e);
+    this.v_x = 0;
+    this.v_y = 0;
+  }
 }
+class Photon extends Particle {
+  constructor(pos_x, pos_y, vel_x, vel_y){
+    super(pos_x, pos_y, vel_x, vel_y, 0, 0);
+    this.decayProbability = 0.1;
+  };
+
+  // decays into an electron/positron pair
+  decay(){
+    let e = new Electron(this.x, this.y, this.v_x/2, this.v_y/2);
+    let p = new Positron(this.x, this.y, this.v_x/2, this.v_y/2);
+    ParticleTracks.activeParticles.push(e,p);
+    this.v_x = 0;
+    this.v_y = 0;
+  }
+};
+
 // Container for particles
 ParticleTracks.activeParticles = [
-  new Muon(110,100, 10, 10),
-  //new Positron(200,100,10,1)
+  //new Positron(200,100,100,5),
+  //new Electron(200,100,100,5),
+  new Photon(500,500,-10,-50)
 ];
+
+ParticleTracks.particleTypes = [Electron, Positron, Muon, Photon];
+
+function createParticle() {
+  let particleClass = selectFromArray(ParticleTracks.particleTypes);
+
+  let x_0 = (2 * Math.random() - 1) * ParticleTracks.width;
+  let y_0 = (2 * Math.random() - 1) * ParticleTracks.height;
+  let v_x = (2 * Math.random() - 1) * 100;
+  let v_y = (2 * Math.random() - 1) * 100;
+
+  vMax = 100;
+  xMax = ParticleTracks.width;
+  yMax = ParticleTracks.height;
+
+  possibleStartingPositions = [
+    //right
+    [0, Math.random()*yMax, Math.random()*vMax, Math.random()*vMax],
+    //top
+    [Math.random()*xMax, 0, Math.random()*vMax, Math.random()*vMax],
+    //left
+    [xMax, Math.random()*yMax, -Math.random()*vMax, Math.random()*vMax],
+    //bottom
+    [Math.random()*xMax, yMax, Math.random()*vMax, -Math.random()*vMax],
+  ];
+
+  startArgs = selectFromArray(possibleStartingPositions);
+
+  ParticleTracks.activeParticles.push(new particleClass(...startArgs));
+};
+
+function selectFromArray(array){
+  return(array[Math.floor(Math.random() * array.length)]);
+};
