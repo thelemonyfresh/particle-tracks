bdf09154446574eae5ac37519d45ee43bbd34c2a
Daniel Porter <daniel@pakled.local>
2019-06-13 13:01:46 +0200

    Tweaks to particle paths.

--- particle_tracks.js
+++ particle_tracks.js
@@ -8,36 +8,76 @@ let svg = document.getElementById("particle-tracks-svg");
 let filterTurbulence = document.getElementById('bg-turbulence');
 
 let canvas = document.getElementById("particles-canvas");
-ctx = canvas.getContext("2d");
+let ctx = prepareCanvas(canvas);
+const dpRatio = window.devicePixelRatio;
 
-
-function draw(x_i, y_i, x_f, y_f) {
-  ctx.lineWidth = 1;
+function draw(x_i, y_i, x_f, y_f, lineWidth) {
+  ctx.lineWidth = lineWidth;
+  ctx.lineCap = 'round';
   ctx.beginPath();
+
   let elementRect = canvas.getBoundingClientRect();
 
   ctx.moveTo(x_i - elementRect.left, y_i - elementRect.top);
   ctx.lineTo(x_f - elementRect.left, y_f - elementRect.top);
-  ctx.strokeStyle = 'blue';
-  ctx.lineWidth = 5;
+
+  // calculate correct gradient points
+  dx = x_i-x_f;
+  dy = y_i-y_f;
+  dist = Math.sqrt(dx*dx + dy*dy);
+  dx = dx/dist;
+  dy = dy/dist;
+  x3 = x_i + (lineWidth/2)*dy;
+  y3 = y_i - (lineWidth/2)*dx;
+  x4 = x_i - (lineWidth/2)*dy;
+  y4 = y_i + (lineWidth/2)*dx;
+
+  // stroke using a linear gradient
+  let gradient = ctx.createLinearGradient(x4,y4,x3,y3);
+
+  gradient.addColorStop(0,'#ffffff00');
+  gradient.addColorStop(0.25,'black');
+  gradient.addColorStop(0.45,'#01A1FE');
+  gradient.addColorStop(0.55,'#01A1FE');
+
+  gradient.addColorStop(0.75,'black');
+  gradient.addColorStop(1,'#ffffff00');
+
+  ctx.strokeStyle = gradient;
+  ctx.lineWidth = lineWidth;
   ctx.stroke();
   ctx.closePath();
 }
 
-function updateCanvasSize(){
+var w = 0;
+var h = 0;
+
+function prepareCanvas(canvasElem) {
   // Initialize canvas to the correct size
-  const rect = canvas.getBoundingClientRect();
-  const dpRatio = window.devicePixelRatio;
+  // https://github.com/zxlee618/drawing-on-a-html-canvas/blob/master/js/index.js
+  const dpr = window.devicePixelRatio || 1;
+  const rect = canvasElem.getBoundingClientRect();
+
 
   // TODO: store canvas on resize and redraw existing state, so we can redraw all the time:
   // https://stackoverflow.com/questions/5517783/preventing-canvas-clear-when-resizing-window
-  canvas.width = rect.width * dpRatio;
-  canvas.height = rect.height * dpRatio;
-  w = canvas.width;
-  h = canvas.height;
+  canvasElem.width = rect.width * dpr;
+  canvasElem.height = rect.height * dpr;
+
+  let context = canvasElem.getContext("2d");
+  context.scale(dpr, dpr);
+
+  w = rect.width;
+  h = rect.height;
+
+  console.log(`canvas w: ${canvas.width}`);
+  console.log(`context w: ${context.width}`);
+  console.log(`dpi: ${dpr}`);
+
+
+  return context;
 }
 
-updateCanvasSize();
 
 class Particle {
   constructor(pos_x, pos_y, vel_x, vel_y) {
@@ -52,14 +92,12 @@ class Particle {
   };
 
   updatePosition() {
+    //console.log(`x: ${this.x}, y: ${this.y}`);
     this.x = this.x_f;
     this.y = this.y_f;
 
-    let new_x = this.x + this.v_x;
-    let new_y = this.y + this.v_y;
-
-    this.x_f = new_x;
-    this.y_f = new_y;
+    this.x_f = this.x + this.v_x;
+    this.y_f = this.y + this.v_y;
   };
 }
 
@@ -68,15 +106,15 @@ function drawPaths() {
   //updateCanvasSize();
   particles.forEach(part => {
     part.updatePosition();
-    draw(part.x,part.y,part.x_f,part.y_f);
+    draw(part.x,part.y,part.x_f,part.y_f, 7);
   });
 };
 
 
 // Container for particles
 let particles = [
-  new Particle(w/2,h/2, 2, 2),
-  new Particle(w/5,h/5, 4, 1)
+  new Particle(0,0, 2, 2),
+  new Particle(w/2,h/2, 3, 1),
 ];
 
 let seed = 1;
@@ -88,9 +126,13 @@ function getSeed(){
 
 function advanceFrame(){
   drawPaths();
+};
+
+function updateBg() {
   filterTurbulence.setAttribute('seed',getSeed());
 };
 
 svg.addEventListener("load",function(){
-  setInterval(advanceFrame,250);
+  setInterval(advanceFrame,25);
+  setInterval(updateBg,250);
 });
