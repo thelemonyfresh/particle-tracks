e03198a3d65c224002c5844a3adcca25762482dc
Daniel Porter <daniel@pakled.local>
2019-06-14 14:06:58 +0200

    Clean up code.

--- particle_tracks.js
+++ particle_tracks.js
@@ -18,19 +18,23 @@ window.onload = function() {
   ParticleTracks.ctx.fillStyle = "yellow";
   ParticleTracks.ctx.fillRect(0, 0, ParticleTracks.canvas.width, ParticleTracks.canvas.height);
 
-  setInterval(drawPaths,25);
-  setInterval(updateBg,500);
+  ParticleTracks.perlinSeed = 1;
+
+  setInterval(drawParticles,25);
+  setInterval(updateNoise,500);
   setInterval(fadeOut,250);
   setInterval(createParticle,2000 + Math.random()*1000);
+
+  // Container for particles
+  ParticleTracks.activeParticles = [];
 };
 
 function prepareCanvas(canvasElem) {
   // Initialize canvas to the correct size
-  // https://github.com/zxlee618/drawing-on-a-html-canvas/blob/master/js/index.js
   const dpr = window.devicePixelRatio || 1;
   const rect = canvasElem.getBoundingClientRect();
 
-  // TODO: store canvas on resize and redraw existing state, so we can redraw all the time:
+  // TODO: store canvas redraw after resize instead of sclaing:
   // https://stackoverflow.com/questions/5517783/preventing-canvas-clear-when-resizing-window
   canvasElem.width = rect.width * dpr;
   canvasElem.height = rect.height * dpr;
@@ -44,35 +48,39 @@ function prepareCanvas(canvasElem) {
   return context;
 }
 
-// Drawing functions.
-function draw(particle) {
-  console.log(particle.constructor.name);
-  //console.log(`absvel: ${particle.absVelocity()}`);
-  // don't draw massless particles
-  if (particle.mass == 0 || particle.absVelocity() == 0) return;
+function drawParticles() {
+  ParticleTracks.activeParticles.forEach(particle => {
+    draw(particle);
+    particle.update();
+  });
+};
 
-  console.log("still drawing");
+// Set up and draw a particle for one frame of movement.
+function draw(particle) {
+  // Don't draw massless particles, stopped particles
+  if (particle.mass == 0 || particle.stopped()) return;
 
   let ctx = ParticleTracks.ctx;
+
+  // Line Width based on particle size, z position
   let lineWidth = 10 + (0.0075 * particle.mass) - particle.z;
-  //console.log(`lw: ${lineWidth}`);
-  //ctx.lineWidth = lineWidth;
+  ctx.lineWidth = lineWidth;
   ctx.lineCap = 'round';
   ctx.beginPath();
 
   let elementRect = ParticleTracks.canvas.getBoundingClientRect();
-
   ctx.moveTo(particle.x_prev, particle.y_prev);
   ctx.lineTo(particle.x, particle.y);
 
   ctx.strokeStyle = particleTrackGradient(particle.x_prev, particle.x, particle.y_prev, particle.y, lineWidth);
-  ctx.lineWidth = lineWidth;
+
   ctx.stroke();
   ctx.closePath();
 }
 
 function particleTrackGradient(x_i,x_f,y_i,y_f,lineWidth) {
-  // calculate and prepare linear gradient perpendicular to particle path
+  // prepare linear gradient perpendicular to particle path (for nice
+  //   blue-to-black effect on particle tracks)
   dx = x_i-x_f;
   dy = y_i-y_f;
   dist = Math.sqrt(dx*dx + dy*dy);
@@ -83,61 +91,53 @@ function particleTrackGradient(x_i,x_f,y_i,y_f,lineWidth) {
   x4 = x_i - (lineWidth/2)*dy;
   y4 = y_i + (lineWidth/2)*dx;
 
-  // particle tracks are blue in the center, with rough black edges
+  // Particle tracks are blue in the center, with fading black edges
   let gradient = ParticleTracks.ctx.createLinearGradient(x4,y4,x3,y3);
   gradient.addColorStop(0,'#ffffff00');
-  gradient.addColorStop(0.25 + Math.random()*0.1,'black');
+  gradient.addColorStop(0.3 - Math.random()*0.05,'black');
   gradient.addColorStop(0.45,'#01A1FE');
   gradient.addColorStop(0.55,'#01A1FE');
-  gradient.addColorStop(0.75 + Math.random()*0.1,'black');
+  gradient.addColorStop(0.7 + Math.random()*0.05,'black');
   gradient.addColorStop(1,'#ffffff00');
 
   return gradient;
 };
 
+// Continuously fade particle tracks.
 function fadeOut() {
   ParticleTracks.ctx.fillStyle = "rgba(255,255,0,0.2)";
   ParticleTracks.ctx.fillRect(0, 0, ParticleTracks.canvas.width, ParticleTracks.canvas.height);
-  //ParticleTracks.ctx.globalAlpha = 0.5;
-  //ParticleTracks.ctx.drawImage(ParticleTracks.svg, 0, 0);
-}
-
-function drawPaths() {
-  ParticleTracks.activeParticles.forEach(particle => {
-    draw(particle);
-    particle.updatePosition();
-
-  });
 };
 
 // Make the Perlin noise background dynamic
-let seed = 1;
-function getSeed(){
-  seed = (seed + 1) % 100;
-  return seed;
+function getPerlinSeed(){
+  ParticleTracks.perlinSeed = (ParticleTracks.perlinSeed + 1) % 100;
+  return ParticleTracks.perlinSeed;
 };
 
-function updateBg() {
-  filterTurbulence.setAttribute('seed',getSeed());
+function updateNoise() {
+  filterTurbulence.setAttribute('seed',getPerlinSeed());
 };
 
 // Particles
 class Particle {
   constructor(x_0, y_0, vel_x, vel_y, charge, mass) {
+    // Physical properties
+    this.mass = mass;
+    this.charge = charge;
+    this.chargeToMassRatio = charge/mass;
+
+    // Last position, velocity, current position
     this.x_prev = x_0;
     this.y_prev = y_0;
 
     this.v_x = vel_x;
     this.v_y = vel_y;
 
-
-    this.mass = mass;
-    this.charge = charge;
-
-    this.chargeToMassRatio = charge/mass;
-
     this.x = this.x_prev + this.v_x;
     this.y = this.y_prev + this.v_y;
+
+    // Random z position, z velocity opposite to z position direction.
     this.z = Math.random();
     this.v_z = 0.1*Math.random() * -1 * this.z/Math.abs(this.z);
   };
@@ -146,60 +146,56 @@ class Particle {
     return(Math.sqrt(Math.pow(this.v_x, 2) + Math.pow(this.v_y, 2)));
   };
 
-  updatePosition() {
-    // possibly particle decay
+  stopped() {
+    return this.absVelocity() <= 2.5;
+  };
+
+  update() {
+    // Possibly particle decay
     if(this.decayProbability != undefined && this.decayProbability > Math.random()){
       this.decay();
       return;
     }
 
-    // stop rending this particle if out of bounds or stopped
+    // Stop rending this particle if out of bounds or stopped
     let out_of_x = this.x > 2 * ParticleTracks.width || this.x < -1 * ParticleTracks.width;
     let out_of_y = this.y > 2* ParticleTracks.height || this.y < -1 * ParticleTracks.height;
-    let stopped = this.absVelocity() <= 2;
-    if( out_of_x || out_of_y || stopped){
-      // remove from list of active particles if way off canvas
+
+    if( out_of_x || out_of_y || this.stopped()){
       let index = ParticleTracks.activeParticles.indexOf(this);
+      // Remove from list of active particles
       ParticleTracks.activeParticles.splice(index,1);
       return;
     }
 
-    //console.log(`x: ${this.x}, y: ${this.y}`);
-    //console.log(`vx: ${this.v_x}, vy: ${this.v_y}`);
-
     this.x_prev = this.x;
     this.y_prev = this.y;
 
-    // update the position
+    // Update the position
     this.x = this.x_prev + this.v_x;
     this.y = this.y_prev + this.v_y;
     this.z = this.z + this.v_z;
 
-    // don't update velocity for massless particles
+    // Update velocity (but not for massless particles)
     if (this.mass != 0) {
+      // Magnetic field
+      let B_x = 0.1;
+      let B_y = 0.125;
 
-      // v_z TEST
-
-      // update the velocity
-      let chargeToMassRatio = this.charge/this.mass;
-
-      let B_x = 0.1; //+ 0.02*Math.abs(this.x /  ParticleTracks.width );
-      let B_y = 0.11; //+ 0.02*Math.abs(this.y / ParticleTracks.height );
-
+      // Acceleration
       let a_x = this.chargeToMassRatio * this.v_y * B_x;
       let a_y = -1 * this.chargeToMassRatio * this.v_x * B_y;
 
-      let drag = 0.8; // * (1/this.mass) * (Math.pow(this.v_x, 2) + Math.pow(this.v_y, 2));
-      console.log(`drag: ${drag}`);
+      // "Drag" -- velocity lost to particle interactions
+      let drag = 0.8;
+
+      // Drag should always be opposite to motion
       let sign_x = this.v_x == 0 ? 1 : this.v_x / Math.abs(this.v_x);
       let sign_y = this.v_y == 0 ? 1 : this.v_y / Math.abs(this.v_y);
 
+      // Update velocity
       this.v_x = this.v_x + a_x - drag * sign_x;
       this.v_y = this.v_y + a_y - drag * sign_y;
-
-      console.log("AFTER");
-      console.log(`delt vx: ${a_x}`);
-      console.log(`delt vy: ${a_y}`);
     }
   };
 };
@@ -219,12 +215,11 @@ class Positron extends Particle {
 class Muon extends Particle {
   constructor(pos_x, pos_y, vel_x, vel_y){
     super(pos_x, pos_y, vel_x, vel_y, -1, 500);
-    this.decayProbability = 0.01;
+    this.decayProbability = 0.02;
   }
 
-  // decays into one electron
+  // Decays into one electron (and some neutrinos which we ignore)
   decay() {
-    console.log("muon decay");
     let e = new Electron(this.x, this.y, this.v_x*0.5, this.v_y*0.5);
     ParticleTracks.activeParticles.push(e);
     this.v_x = 0;
@@ -235,10 +230,10 @@ class Muon extends Particle {
 class Photon extends Particle {
   constructor(pos_x, pos_y, vel_x, vel_y) {
     super(pos_x, pos_y, vel_x, vel_y, 0, 0);
-    this.decayProbability = 0.1;
+    this.decayProbability = 0.3;
   };
 
-  // decays into an electron/positron pair
+  // Really some hand-wavy pair production that looks cool
   decay(){
     let e = new Electron(this.x, this.y, this.v_x*0.7, this.v_y*0.7);
     let p = new Positron(this.x, this.y, this.v_x*0.7, this.v_y*0.7);
@@ -257,10 +252,10 @@ class Proton extends Particle {
 class Neutron extends Particle {
   constructor(pos_x, pos_y, vel_x, vel_y) {
     super(pos_x, pos_y, vel_x, vel_y, 0, 900);
-    this.decayProbability = 0.1;
+    this.decayProbability = 0.05;
   };
 
-  // free neutron beta decay
+  // Free neutron beta decay
   decay() {
     let e = new Electron(this.x, this.y, this.v_x*0.6, this.v_y*0.6);
     let p = new Proton(this.x, this.y, this.v_x*0.7, this.v_y*0.7);
@@ -268,45 +263,39 @@ class Neutron extends Particle {
     this.v_x = 0;
     this.v_y = 0;
   }
-}
+};
 
-// Container for particles
-ParticleTracks.activeParticles = [
-  //new Positron(200,100,100,5),
-  //new Electron(200,100,100,5),
-  //new Neutron(0,500,50,0)
-];
-
-
-ParticleTracks.particleTypes = [Muon, Muon, Photon, Proton, Neutron];
+ParticleTracks.particleTypes = [Muon, Photon, Proton, Neutron];
 
 function createParticle() {
   let particleClass = selectFromArray(ParticleTracks.particleTypes);
 
-  let x_0 = (2 * Math.random() - 1) * ParticleTracks.width;
-  let y_0 = (2 * Math.random() - 1) * ParticleTracks.height;
-  let v_x = (2 * Math.random() - 1) * 100;
-  let v_y = (2 * Math.random() - 1) * 100;
-
   // TODO: choose random energies, use momentum to decide velocity
-  vMax = 25;
-  vMin = 20;
-  xMax = ParticleTracks.width;
-  yMax = ParticleTracks.height;
+  let vMax = 30;
+  let vMin = 20;
+
+  randomV = vMin + Math.random()*vMax;
+  randomV2 = vMin + Math.random()*vMax;
+
+  let xMax = ParticleTracks.width;
+  let yMax = ParticleTracks.height;
+
+  let randomX = xMax * Math.random();
+  let randomY = yMax * Math.random();
+
 
   possibleStartingPositions = [
-    //right
-    [0, Math.random()*yMax, vMin + Math.random()*vMax, vMin + (0.25 + 0.5*Math.random())*vMax],
-    //top
-    [Math.random()*xMax, 0, vMin + (0.25 + 0.5*Math.random())*vMax, vMin + Math.random()*vMax],
     //left
-    [xMax, Math.random()*yMax, -1*(vMin + Math.random()*vMax), 0.25 + 0.5*Math.random()*vMax],
+    [0, randomY, randomV, randomV2],
+    //top
+    [randomX, 0, randomV2, randomV],
+    //right
+    [xMax, randomY, -randomV, randomV2],
     //bottom
-    [Math.random()*xMax, yMax, vMin + (0.25 + 0.5*Math.random())*vMax, -(vMin + Math.random()*vMax)],
+    [randomX, yMax, -randomV2, randomV],
   ];
 
   startArgs = selectFromArray(possibleStartingPositions);
-
   ParticleTracks.activeParticles.push(new particleClass(...startArgs));
 };
 
